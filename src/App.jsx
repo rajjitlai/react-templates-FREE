bb31c151e738a7ea6723bb553d7b2718747343c4
RAJJIT <2.0lairaj@gmail.com>
2024-05-29 22:18:13 +0530

    template-dropdown_05-need-fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { DropDown_01 } from "./Components/DropDown_01/DropDown_01"
 import DropDown_02 from "./Components/DropDown_02/DropDown_02"
 import { DropDown_03 } from "./Components/DropDown_03/DropDown_03"
 import { DropDown_04 } from "./Components/DropDown_04/DropDown_04"
+import DropDown_05 from "./Components/DropDown_05/DropDown_05"
 import Menu_01 from "./Components/Menu_01/Menu_01"
 import Menu_02 from "./Components/Menu_02/Menu_02"
 import "./global.css"
@@ -25,7 +26,10 @@ const App = () => {
       {/* <DropDown_03 /> */}
 
       {/* DropDown_04 */}
-      <DropDown_04 />
+      {/* <DropDown_04 /> */}
+
+      {/* DropDown_05 */}
+      <DropDown_05 />
     </>
   )
 }
--- src/Components/DropDown_05/DropDown_05.jsx
+++ src/Components/DropDown_05/DropDown_05.jsx
@@ -1,5 +1,7 @@
 import { useRef, useState } from 'react'
 import "./dropdown_05.css"
+import chevron_right from "../../assets/chevron_right.svg"
+import arrow_back from "../../assets/arrow_back.svg"
 
 const items = [
     {
@@ -12,25 +14,74 @@ const items = [
 ]
 
 const MenuButton = ({
-    name, 
-    icon, 
-    index, 
+    name,
+    icon,
+    index,
     hasSubItems,
     subMenuHeight,
     onClick,
 }) => {
     return (
-        <button onClick={() => onClick ? onClick(index, subMenuHeight): null}>
+        <button onClick={() => onClick ? onClick(index, subMenuHeight) : null}>
             <span>{icon || name}</span>
             {name}
-            {hasSubItems && <span>chevron_right</span>}
+            {hasSubItems && <span>{chevron_right}</span>}
         </button>
     )
 }
 
-const DropDown_05 = () => {
+const MenuItem = ({
+    name,
+    index,
+    activeSubMenu,
+    subItems,
+    onClick,
+}) => {
+    const subMenuRef = useRef(null);
+    const isActive = activeSubMenu === index
     return (
-        <div>DropDown_05</div>
+        <>
+            <MenuButton onClick={subItems ? onClick : () => null} name={name} index={index} hasSubItems={Boolean(subItems)} subMenuHeight={subMenuRef.current?.clientHeight} />
+            {subItems?.length && (
+                <nav ref={subMenuRef} className={`sub-menu ${isActive ? "open" : ""}`}>
+                    <MenuButton onClick={onClick} icon={arrow_back} name={name} />
+                    {subItems.map((subItem) => (
+                        <MenuButton key={subItem} name={subItem} />
+                    ))}
+                </nav>
+            )}
+        </>
+    )
+}
+
+const DropDown_05 = () => {
+    const [isOpen, setIsOpen] = useState(false)
+    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false)
+    const [subMenuHeight, setSubMenuHeight] = useState()
+    const [activeSubMenu, setActiveSubMenu] = useState()
+    const handleClick = (index, subMenuHeight) => {
+        if (index > -1)
+            setActiveSubMenu(index)
+        setSubMenuHeight(subMenuHeight)
+        setIsSubMenuOpen(index > -1)
+    }
+
+    return (
+        <div className={`dropdown ${isOpen ? 'open' : ''}`}>
+            <button onClick={() => setIsOpen(!isOpen)}>
+                Preferences
+            </button>
+
+            <div style={{ height: `${subMenuHeight || 168}px` }} className='menu'>
+                <div className={`menu-inner ${isSubMenuOpen ? 'open' : ''}`}>
+                    <nav className='main-menu'>
+                        {items.map((item, index) => (
+                            <MenuItem key={item.name} name={item.name} index={index} activeSubMenu={activeSubMenu} subItems={item.subItems} onClick={handleClick} />
+                        ))}
+                    </nav>
+                </div>
+            </div>
+        </div>
     )
 }
 
