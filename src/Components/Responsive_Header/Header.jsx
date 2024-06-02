74dcf052738914791749aac78bd7e1a4fafcdbac
RAJJIT <2.0lairaj@gmail.com>
2024-06-02 23:00:25 +0530

    template-header-01-v3

--- src/Components/Responsive_Header/Header.jsx
+++ src/Components/Responsive_Header/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { delay, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 import "./header.css"
 
@@ -25,35 +25,35 @@ const Header = () => {
             }
         })
     }
+
+    const navLinks = [
+        { label: "Home", icon: "" },
+        { label: "Projects", icon: "" },
+        { label: "Blog", icon: "" },
+        { label: "Testimonials", icon: "" },
+        { label: "About Us", icon: "" },
+        { label: "Contact Us", icon: "" },
+    ]
+
+    return (
+        <div className="header">
+            <div className="header__Logo">
+                <img src="" alt="logo" />
+                <h1>Head</h1>
+            </div>
+            <div className="header__Icon">
+                <p onClick={() => setIsOpen(isOpen => !isOpen)}>C</p>
+            </div>
+            <motion.ul className="header__Nav" animate={isOpen ? "open" : "closed"} variants={navbar} >
+                {navLinks.map((link) => (
+                    <li className="nItem" key={link.label} onClick={() => setIsOpen(isOpen => !isOpen)}>
+                        <p className={link.icon}>c</p>
+                        {link.label}
+                    </li>
+                ))}
+            </motion.ul>
+        </div>
+    )
 }
-
-const navLinks = [
-    { label: "Home", icon: "" },
-    { label: "Projects", icon: "" },
-    { label: "Blog", icon: "" },
-    { label: "Testimonials", icon: "" },
-    { label: "About Us", icon: "" },
-    { label: "Contact Us", icon: "" },
-]
-
-return (
-    <div className="header">
-        <div className="hLogo">
-            <img src="" alt="logo" />
-            <h1>Head</h1>
-        </div>
-        <div className="hIcon">
-            <p onClick={() => setIsOpen(isOpen => !isOpen)}>C</p>
-        </div>
-        <motion.ul className="hNav" animate={isOpen ? "open" : "closed"} variants={navbar} >
-            {navLinks.map((link) => (
-                <li className="nItem" key={link.label} onClick={() => setIsOpen(isOpen => !isOpen)}>
-                    <p className={link.icon}>c</p>
-                    {link.label}
-                </li>
-            ))}
-        </motion.ul>
-    </div>
-)
 
 export default Header
