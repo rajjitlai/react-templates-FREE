45711dc5bdc25476d1a1cecd6e781ec9c5e5078c
RAJJIT <2.0lairaj@gmail.com>
2024-06-04 23:31:41 +0530

    cleanup-01

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Carousel from "./Components/Image_Slider_01/Image_Slider_01"
 import "./global.css"
 
 const App = () => {
   return (
     <>
       {/* Render your component here!!! */}
-      <Carousel />
     </>
   )
 }
