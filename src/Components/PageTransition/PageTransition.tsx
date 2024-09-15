daa7e7885897b5421d7a0a6fd81de606bc9a47b3
RAJJIT <2.0lairaj@gmail.com>
2024-09-15 21:24:36 +0530

    added-page-transitions

---
+++ src/Components/PageTransition/PageTransition.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route, Outlet, useNavigate } from 'react-router-dom'
+import "./transition.css"
+
+const Home = () => <main>Home</main>
+const About = () => <main>About</main>
+const Work = () => <main>Work</main>
+
+const Bars = () => {
+    <div id='bars' className='bars'>
+        <div />
+        <div style={{ animationDelay: "0.1s" }} />
+        <div style={{ animationDelay: "0.3s" }} />
+        <div style={{ animationDelay: "0.5s" }} />
+        <div style={{ animationDelay: "0.8s" }} />
+    </div>
+}
+
+const Link = ({ to, children }) => {
+    const navigate = useNavigate()
+    const handleClicked = () => {
+        const bars = document.getElementById("bars")
+
+        bars?.classList.add("show")
+
+        setTimeout(() => {
+            bars?.classList.remove("show")
+            bars?.classList.add("hide")
+            navigate(to)
+        }, 800)
+    }
+
+    return (
+        <a onClick={handleClicked}>
+            {children}
+        </a>
+    )
+}
+
+const Layout = () => {
+    <>
+        <nav>
+            <h1>Portfolio</h1>
+            <ul>
+                <li>
+                    <Link to="/home">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/work">Work</Link>
+                </li>
+            </ul>
+        </nav>
+        <Bars />
+        <Outlet />
+    </>
+}
+
+const PageTransition = () => {
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Layout />}>
+                <Route path='home' element={<Home />} />
+                <Route path='about' element={<About />} />
+                <Route path='work' element={<Work />} />
+            </Route>
+        </Routes>
+    </BrowserRouter>
+}
+
+export default PageTransition
